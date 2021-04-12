305ebb1ef4cdb2e48039bd658cbc59a4184a21d7
vlnn <anohin.va@gmail.com>
2021-04-12 19:49:49 +0300

    Add eldev

---
+++ tests/test-za.el
@@ -0,0 +1,26 @@
+;;; test-za.el --- Description -*- lexical-binding: t; -*-
+;;
+;; Copyright (C) 2021 vlnn
+;;
+;; Author: vlnn <https://github.com/vlnn>
+;; Created: April 12, 2021
+;; Modified: April 12, 2021
+;; Version: 0.0.1
+;; Homepage: https://github.com/vlnn/test-za
+;; Package-Requires: ((emacs "24.3"))
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; Commentary:
+;;
+;;  Description
+;;
+;;; Code:
+
+(require 'buttercup)
+
+(describe "Function: `sum'"
+  (it "should work :)"
+    (expect (sum 3 7) :to-be 10)
+    (expect (sum 3 7) :not :to-be 11)))
+;;; test-za.el ends here
